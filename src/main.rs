189ef62be594283fac73ff1f833af8d602b003cf
reptpr <sadiyamuddu2008@gmail.com>
2026-03-30 23:46:17 +0530

    some cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,35 @@
-use std::env;
-use std::fs;
+use std::error::Error;
+use std::{env, fs, process::exit};
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let config = parse_config(&args);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments {err}");
+        exit(1);
+    });
+    println!("searching for {} in file {}", config.query, config.file);
+    if let Err(e) = run(config) {
+        println!("Error : {e}");
+    }
+}
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.file)?;
+    println!("{}", contents);
+    Ok(())
 }
 struct Config {
     query: String,
     file: String,
 }
-
-fn parse_config(args: &[String]) -> Config {
-    let query = args[1].clone();
-    let file_path = ar[2].clone();
-    Config {
-        query,
-        file: file_path,
+impl Config {
+    fn new(args: &[String]) -> Result<Config, &str> {
+        if args.len() < 2 {
+            print!("Not Enough arguments");
+        }
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+        Ok(Config {
+            query,
+            file: file_path,
+        })
     }
 }
