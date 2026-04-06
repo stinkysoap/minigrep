5ac92ad6c8cf720e6c1587d039db864a842070b2
mudassir <mudassir@iteronas.com>
2026-04-06 14:05:05 +0530

    functional just install the thing from here on

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,9 @@
 use std::error::Error;
-use std::{env, fs, process::exit};
-
+use std::{env, fs};
 pub struct Config {
     pub query: String,
     pub file: String,
+    pub ignore_case: bool,
 }
 
 impl Config {
@@ -11,21 +11,30 @@ impl Config {
         if args.len() < 3 {
             return Err("Not enough arguments");
         }
+        let ignore_case = env::var("IGNORE_CASE").is_ok();
+
         Ok(Config {
             query: args[1].clone(),
             file: args[2].clone(),
+            ignore_case,
         })
     }
 }
-
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.file)?;
-    for line in search(&config.query, &contents) {
-        println!("{}", line);
+
+    let results = if config.ignore_case {
+        search_case_insensitive(&config.query, &contents)
+    } else {
+        search(&config.query, &contents)
+    };
+
+    for line in results {
+        println!("{line}");
     }
+
     Ok(())
 }
-
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     let mut results = Vec::new();
     for line in contents.lines() {
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,13 @@
 use minigrep::{Config, run};
-use std::error::Error;
 use std::{env, fs, process::exit};
 fn main() {
     let args: Vec<String> = env::args().collect();
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments {err}");
+        eprintln!("Problem parsing arguments {err}");
         exit(1);
     });
     println!("searching for {} in file {}", config.query, config.file);
     if let Err(e) = run(config) {
-        println!("Error : {e}");
+        eprintln!("Error : {e}");
     }
 }
